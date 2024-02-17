f606c2e57c410c3fe71ca1769cd5681dbdd16c6d
Yizhuo Liang <yizhuo.liang.hello@gmail.com>
2024-02-17 16:30:29 -0500

    update

---
+++ serializer/serializer.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	pb "github.com/yizhuoliang/J2KSerializer"
+
+	"google.golang.org/grpc"
+)
+
+type server struct {
+	pb.UnimplementedPongServiceServer
+}
+
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	log.Printf("Received: %v", in.GetMessage())
+	return &pb.HelloReply{Message: "Hello " + in.GetSenderId()}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterPongServiceServer(s, &server{})
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
